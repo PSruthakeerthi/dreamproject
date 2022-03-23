5964b404c15eb6c6b5ec060d0284534279c3a6e7
PSruthakeerthi <93244566+PSruthakeerthi@users.noreply.github.com>
2022-03-23 21:28:22 +0530

    Add files via upload

---
+++ project.js
@@ -0,0 +1,15 @@
+import React from "react";
+function Project() {
+  return (
+    <>
+      <h1 data-ns-test="project-name">Age Calculator</h1>
+      <p data-ns-test="project-description">
+        {" "}
+        This is a calculator to calculate our age. User enter his date of birth
+        in dd-mm-yy format and he gets his0 age in years, months and days.{" "}
+      </p>
+    </>
+  );
+}
+
+export default Project;
